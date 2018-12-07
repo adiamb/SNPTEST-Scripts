cf862ffee9ddd63b3f259a72133799343dce94f8
Aditya Ambati <ambati@stanford.edu>
2018-12-06 19:29:19 -0800

    Update split_impute.py

--- split_impute.py
+++ split_impute.py
@@ -3,12 +3,20 @@ import subprocess
 from subprocess import PIPE
 import sys
 import time
+import argparse
+import gzip
+
 
 
 
 def get_wc(impute):
 	global stdout_
-	wc_ = subprocess.Popen('wc -l < '+impute, shell=True, stdout=PIPE, stderr=PIPE)
+	if '.gz' in impute:
+		print 'Detected an gzipped {} , please wait while lines are being counted'.format(impute)
+		wc_command = 'zcat '+impute+ ' | wc -l'
+	else:
+		wc_command = 'wc -l < '+impute
+	wc_ = subprocess.Popen(wc_command, shell=True, stdout=PIPE, stderr=PIPE)
 	stdout, stderr = wc_.communicate()
 	if stderr:
 		print 'error try again not a valid file or check directory for file'
@@ -18,47 +26,61 @@ def get_wc(impute):
 
 def make_file(chunkid, chunk_n):
 	global chunk_filename
-	chunk_filename = chunkid+'_'+str(chunk_n)+'.impute2'
+	chunk_filename = chunkid+'_'+str(chunk_n)+'.impute'
 	#chunk_write = open(chunk_filename, 'w')
 	return(chunk_filename)
 
-def main(argv):
-	impute=sys.argv[1] ## the impute file
-	chunks = int(sys.argv[2]) ## no of chunks 
-	chunkid = sys.argv[3] ## stringID for chunks
+def main():
+	import argparse 
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-impute', help='A impute file in the gen format', required=True)
+	parser.add_argument('-chunks', help='how many chunks you want to divide the files into', required=True)
+	parser.add_argument('-chunkid', help='A string for chunkid', required=True)
+	args=parser.parse_args()
+	impute=args.impute ## the impute file
+	chunks = int(args.chunks) ## no of chunks 
+	chunkid = args.chunkid ## stringID for chunks
 	
 	line_num = int(get_wc(impute))
 	lines_chunk = line_num/chunks
-	remainder = float(line_num) % float(chunks)
+	print 'REQUESTED CHUNKS {} CALCULATED LINE LOAD PER CHUNK AS {} '.format(chunks, lines_chunk)
+	#remainder = float(line_num) % float(chunks)
 	if lines_chunk > line_num:
 		raise ValueError('CHUNK_LINES GREATER THAN TOTAL LINE_NUM')
 
-	processed_buf = 1
-	line_track = 1
+	processed_buf = 0
+	line_track = 0
 	chunk_track = 1
-	lines_for_chunk =1
+	lines_for_chunk =0
 
 
 	outfile_name = make_file(chunkid, chunk_track)
-	outfile = open(outfile_name, 'wr')
-	with open(impute) as in_file:
-		for n, line in enumerate(in_file):
-			line_track += 1
-			#lines_for_chunk += 1
-			if line_track == 10000:
-				processed_buf += 10000
-				print "PROCESSED LINES ", processed_buf, (float(processed_buf)/float(line_num))*100, " percent done FROM ", impute
-				line_track = 1
-			if lines_for_chunk == lines_chunk:
-				chunk_track += 1
-				outfile_name_new=make_file(chunkid =chunkid, chunk_n =chunk_track)
-				outfile = open(outfile_name_new, 'wr')
-				lines_for_chunk = 1
-			else:
-				outfile.write(line)
-				lines_for_chunk += 1
+	outfile = open(outfile_name, 'w')
+	if 'gz' in impute:
+		file_handle = gzip.open(impute, 'rb')
+	else:
+		file_handle = open(impute, 'r')
+	
+	for n, line in enumerate(file_handle):
+		line_track += 1
+		#lines_for_chunk += 1
+		if line_track == 1000:
+			processed_buf += 1000
+			print ' <<<<<  {}  PROCESSED LINES  {} % percent done from {} >>>>>'.format(processed_buf, (float(processed_buf)/float(line_num))*100, impute)
+			line_track = 1
+		if lines_for_chunk == lines_chunk:
+			#outfile.close()
+			chunk_track += 1
+			outfile_name_new=make_file(chunkid =chunkid, chunk_n =chunk_track)
+			outfile = open(outfile_name_new, 'w')
+			lines_for_chunk = 0
+		else:
+			outfile.write(line)
+			lines_for_chunk += 1
+	#outfile.close()
+	file_handle.close()
 	
 #		if chunk_track == chunks/2:
 #			print '<<<<<<< PROGRESS - 50% DONE >>>>>>>'
 
-if __name__ == "__main__": main(sys.argv)
+if __name__ == "__main__": main()
